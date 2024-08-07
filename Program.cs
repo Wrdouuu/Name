9b953d557469cd7f5fc387fa39138ebcafa3fd6d
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-07 12:34:35 +0700

    name

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HIENTHILOICHAO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            string yourname = Console.ReadLine();
+            Console.WriteLine("Hello " + yourname);
+
+        }
+    }
+
+}
+    
+
